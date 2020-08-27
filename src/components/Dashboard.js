eacecf8255d48b20dd6161556988b032fe39e7d4
Yannik Sood <yanniksood7@gmail.com>
2020-08-27 00:05:54 -0400

    heroku configurations set

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -21,20 +21,21 @@ const Dashboard = () => (
         </Col>
       </Row>
       <Row>
-        <Col xs={12} md={4}>
+        <Col  md={12} lg={4}>
           <h3><Badge variant="light">Biggest Movers Today</Badge></h3>
           <MarketDataWidget />
         </Col>
        
-        <Col xs={12} md={4}>
+        <Col  md={12} lg={4}>
           <h3><Badge variant="light">Market Overview</Badge></h3>
           <MarketOverviewWidget />
         </Col>
 
-        <Col xs={12} md={4}>
+        <Col  md={12} lg={4}>
           <h3><Badge variant="light">Upcoming Economic Events</Badge></h3>
           <EconDataWidget />
         </Col>
+        
       </Row>
     </Container>
   </div>
