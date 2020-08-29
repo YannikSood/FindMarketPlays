f59016bbc51c4749eb86ad14dc88f9f42ba09a11
Heyitswilson <wilsonngu13@gmail.com>
2020-08-29 13:09:30 -0700

    Cleaned up code on memory leak fix in UnusualOptions component

--- src/components/Options/UnusualOptions.js
+++ src/components/Options/UnusualOptions.js
@@ -33,19 +33,6 @@ const UnusualOptions = ({ isAuthed }) => {
     }
   }, [isAuthed, history, searchedValue]);
 
-  // useEffect(() => {
-  //   const fetchData = () => {
-  //     const url = `https://api.benzinga.com/api/v1/signal/option_activity?page=1&parameters%5Btickers%5D=${searchedValue}&token=bd2570cf59734eb9934b3cd886ce958b`;
-  //     fetch(url, { headers: { Accept: 'application/json' } })
-  //       .then(res => res.json()
-  //         .then((json) => {
-  //           setOptions(json.option_activity || []);
-  //         }))
-  //       .catch(err => console.error(err)); // eslint-disable-line
-  //   };
-  //   debounce(fetchData());
-  // }, [searchedValue]);
-
   // Handlers
   const handleInputChange = (event) => {
     setSearchedValue(event.target.value.toUpperCase());
