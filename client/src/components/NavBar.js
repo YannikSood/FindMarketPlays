e784d33309e5bd6fae9a76b2a6687790001aef23
Heyitswilson <wilsonngu13@gmail.com>
2020-08-31 22:28:36 -0700

    Fixed mobile dropdown

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 //Local Imports
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import * as ROUTES from '../routes/routes';
 import firebase from '../firebase/firebase';
+import { $ } from 'jquery';
 
 // Consider highlighting the active nav link here based on the route param
 
@@ -12,41 +13,71 @@ const Navigation = ({ isAuthed }) => (
   <div>{isAuthed ? <NavigationAuth /> : <NavigationNonAuth />}</div>
 );
 const NavigationAuth = () => (
-  <Navbar collapseOnSelect expand="sm" className="justify-content-center" bg="dark" variant="dark" fixed="top">
+  <Navbar
+    collapseOnSelect
+    expand="sm"
+    className="justify-content-center"
+    bg="dark"
+    variant="dark"
+    fixed="top"
+  >
     <Navbar.Brand href={"/"}>Platform</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav>
-        <NavLink className="nav-link" to={ROUTES.DASHBOARD}>Market Overview</NavLink>
-        <NavLink className="nav-link" to={ROUTES.SINGLE_STOCK_RESEARCH}>Stock Lookup</NavLink>
-        <NavLink className="nav-link" to={ROUTES.DD}>Research </NavLink>
-        <NavLink className="nav-link" to={ROUTES.NOTES}>Notes </NavLink>
-        <NavLink className="nav-link" to={ROUTES.UNUSUAL_OPTIONS}>Options Feed</NavLink>
-        <NavLink className="nav-link" to={ROUTES.NEWS_FEED}>News Feed</NavLink>
-        <NavLink className="nav-link" to={ROUTES.PROFILE}>Account</NavLink>
-        {/* <NavLink className="nav-link" to={ROUTES.ABOUT}>About </NavLink> */}
+        <Nav.Link href={`${ROUTES.DASHBOARD}`}>Market Overview</Nav.Link>
+        <Nav.Link href={`${ROUTES.SINGLE_STOCK_RESEARCH}`}>
+          Stock Lookup
+        </Nav.Link>
+        <Nav.Link href={`${ROUTES.DD}`}>Research </Nav.Link>
+        <Nav.Link href={`${ROUTES.NOTES}`}>Notes </Nav.Link>
+        <Nav.Link href={`${ROUTES.UNUSUAL_OPTIONS}`}>Options Feed</Nav.Link>
+        <Nav.Link href={`${ROUTES.NEWS_FEED}`}>News Feed</Nav.Link>
+        <Nav.Link href={`${ROUTES.PROFILE}`}>Account</Nav.Link>
+        {/* <Nav.Link className="nav-link" to={ROUTES.ABOUT}>About </Nav.Link> */}
       </Nav>
     </Navbar.Collapse>
-    <Button className="ml-2" variant="primary" onClick={() => { firebase.auth().signOut(); }}>Sign Out</Button>
+    <Button
+      className="ml-2"
+      variant="primary"
+      onClick={() => {
+        firebase.auth().signOut();
+      }}
+    >
+      Sign Out
+    </Button>
   </Navbar>
 );
 
 const NavigationNonAuth = () => (
-  <Navbar collapseOnSelect expand="sm" className="justify-content-center" bg="dark" variant="dark" fixed="top">
-    <Navbar.Brand href={'/'}>Platform</Navbar.Brand>
+  <Navbar
+    collapseOnSelect
+    expand="sm"
+    className="justify-content-center"
+    bg="dark"
+    variant="dark"
+    fixed="top"
+  >
+    <Navbar.Brand href={"/"}>Platform</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav>
-        <NavLink className="nav-link" to={ROUTES.DASHBOARD}>Market Overview</NavLink>
-        <NavLink className="nav-link" to={ROUTES.SINGLE_STOCK_RESEARCH}>Stock Lookup</NavLink>
-        <NavLink className="nav-link" to={ROUTES.DD}>Research </NavLink>
-        <NavLink className="nav-link" to={ROUTES.NOTES}>Notes </NavLink>
-        <NavLink className="nav-link" to={ROUTES.UNUSUAL_OPTIONS}>Options Feed</NavLink>
-        <NavLink className="nav-link" to={ROUTES.NEWS_FEED}>News Feed</NavLink>
-        {/* <NavLink className="nav-link" to={ROUTES.ABOUT}>About </NavLink> */}
+        <Nav.Link href={`${ROUTES.DASHBOARD}`}>Market Overview</Nav.Link>
+        <Nav.Link href={`${ROUTES.SINGLE_STOCK_RESEARCH}`}>
+          Stock Lookup
+        </Nav.Link>
+        <Nav.Link href={`${ROUTES.DD}`}>Research </Nav.Link>
+        <Nav.Link href={`${ROUTES.NOTES}`}>Notes </Nav.Link>
+        <Nav.Link href={`${ROUTES.UNUSUAL_OPTIONS}`}>Options Feed</Nav.Link>
+        <Nav.Link href={`${ROUTES.NEWS_FEED}`}>News Feed</Nav.Link>
+        {/* <Nav.Link className="nav-link" to={ROUTES.ABOUT}>About </Nav.Link> */}
       </Nav>
-      </Navbar.Collapse>
-      <NavLink to={ROUTES.LOGIN}><Button className="ml-2" variant="primary">Log In/Sign Up</Button></NavLink>
+    </Navbar.Collapse>
+    <Nav.Link to={ROUTES.LOGIN}>
+      <Button className="ml-2" variant="primary">
+        Log In/Sign Up
+      </Button>
+    </Nav.Link>
   </Navbar>
 );
 const mapStateToProps = (state) => {
