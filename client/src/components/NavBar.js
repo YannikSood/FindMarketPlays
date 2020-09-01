403831fc463423c792694830626953e36e8689a3
Heyitswilson <wilsonngu13@gmail.com>
2020-09-01 11:41:10 -0700

    Fixed navbar active link highlights

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
+import { NavLink, useHistory, withRouter } from 'react-router-dom';
 //Local Imports
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import * as ROUTES from '../routes/routes';
 import firebase from '../firebase/firebase';
-// import { $ } from 'jquery';
 
-// Consider highlighting the active nav link here based on the route param
-
-const Navigation = ({ isAuthed }) => (
-  <div>{isAuthed ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+const Navigation = ({ isAuthed, location }) => (
+  <div>{isAuthed ? NavigationAuth(location): NavigationNonAuth(location)}</div>
 );
-const NavigationAuth = () => {
+const NavigationAuth = (location) => {
   const history = useHistory();
-
   return (
     <Navbar
       collapseOnSelect
@@ -27,7 +23,7 @@ const NavigationAuth = () => {
       <Navbar.Brand href={"/"}>FMP</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav>
+        <Nav activeKey={location.pathname}>
           <Nav.Link className="nav-link" href={`${ROUTES.DASHBOARD}`}>Market Overview</Nav.Link>
           <Nav.Link className="nav-link" href={`${ROUTES.SINGLE_STOCK_RESEARCH}`}>
             Stock Lookup
@@ -54,37 +50,39 @@ const NavigationAuth = () => {
   )
 };
 
-const NavigationNonAuth = () => (
-  <Navbar
-    collapseOnSelect
-    expand="sm"
-    className="justify-content-center"
-    bg="dark"
-    variant="dark"
-    fixed="top"
-  >
-    <Navbar.Brand href={"/"}>FMP</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav>
-        <Nav.Link className="nav-link" href={`${ROUTES.DASHBOARD}`}>Market Overview</Nav.Link>
-        <Nav.Link className="nav-link" href={`${ROUTES.SINGLE_STOCK_RESEARCH}`}>
-          Stock Lookup
-        </Nav.Link>
-        <Nav.Link className="nav-link" href={`${ROUTES.DD}`}>Research </Nav.Link>
-        <Nav.Link className="nav-link" href={`${ROUTES.NOTES}`}>Notes </Nav.Link>
-        <Nav.Link className="nav-link" href={`${ROUTES.UNUSUAL_OPTIONS}`}>Options Feed</Nav.Link>
-        <Nav.Link className="nav-link" href={`${ROUTES.NEWS_FEED}`}>News Feed</Nav.Link>
-        {/* <Nav.Link className="nav-link" to={ROUTES.ABOUT}>About </Nav.Link> */}
-      </Nav>
-    </Navbar.Collapse>
-    <NavLink to={ROUTES.LOGIN}>
-      <Button className="ml-2" variant="primary">
-        Log In / Sign Up
-      </Button>
-    </NavLink>
-  </Navbar>
-);
+const NavigationNonAuth = (location) => {
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="sm"
+      className="justify-content-center"
+      bg="dark"
+      variant="dark"
+      fixed="top"
+    >
+      <Navbar.Brand href={"/"}>FMP</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav activeKey={location.pathname}>
+          <Nav.Link className="nav-link" href={`${ROUTES.DASHBOARD}`}>Market Overview</Nav.Link>
+          <Nav.Link className="nav-link" href={`${ROUTES.SINGLE_STOCK_RESEARCH}`}>
+            Stock Lookup
+          </Nav.Link>
+          <Nav.Link className="nav-link" href={`${ROUTES.DD}`}>Research </Nav.Link>
+          <Nav.Link className="nav-link" href={`${ROUTES.NOTES}`}>Notes </Nav.Link>
+          <Nav.Link className="nav-link" href={`${ROUTES.UNUSUAL_OPTIONS}`}>Options Feed</Nav.Link>
+          <Nav.Link className="nav-link" href={`${ROUTES.NEWS_FEED}`}>News Feed</Nav.Link>
+          {/* <Nav.Link className="nav-link" to={ROUTES.ABOUT}>About </Nav.Link> */}
+        </Nav>
+      </Navbar.Collapse>
+      <NavLink to={ROUTES.LOGIN}>
+        <Button className="ml-2" variant="primary">
+          Log In / Sign Up
+        </Button>
+      </NavLink>
+    </Navbar>
+  )
+};
 const mapStateToProps = (state) => {
   const { auth } = state;
   return {
@@ -92,4 +90,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Navigation);
+export default connect(mapStateToProps)(withRouter(Navigation));
