d5eca0cad95ffd6a854aecde9dee02b2fb0f0136
Heyitswilson <wilsonngu13@gmail.com>
2020-08-31 17:59:34 -0700

    Add keys

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 // NOTE: Before this goes live, these should be set as secret keys in environment variables
 const config = {
-  apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
+  apiKey: `${process.env.FIREBASE_ID}`,
   authDomain: 'findmarketplays-f8556.firebaseapp.com',
   databaseURL: 'https://findmarketplays-f8556.firebaseio.com',
   projectId: 'findmarketplays-f8556',
