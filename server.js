090db05c65372f26c12fd979cafeb2f943b87dbd
Heyitswilson <wilsonngu13@gmail.com>
2020-09-04 15:43:19 -0700

    https testing

--- server.js
+++ server.js
@@ -8,14 +8,19 @@ const path = require('path');
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-    app.get('/', function(req, res) {
-      if (req.protocol === 'http') {
-        res.redirect('https://' + 
-        req.get('host') + req.originalUrl)
-      }
-    })
-}
+
+
+// if (process.env.NODE_ENV === "production") {
+//     app.get('/', function(req, res) {
+
+// req.protocol might be a solution. adding lines 13 through 23 redirected too many times and heroku doesn't load
+
+//       if (req.protocol === 'http') {
+//         res.redirect('https://' + 
+//         req.get('host') + req.originalUrl)
+//       }
+//     })
+// }
 
 // if(process.env.NODE_ENV === 'production') {
 //   app.use((req, res) => {
