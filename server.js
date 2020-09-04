495eec0eca7ebb89a4412204f7fed0a197c55440
Yannik Sood <yanniksood7@gmail.com>
2020-09-04 03:11:18 -0700

    fixing  https

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const path = require('path');
 
 app.use(cors());
 
-if(process.env.NODE_ENV === 'production') {
-  app.use((req, res) => {
-    if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`)
-  })
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use((req, res) => {
+//     if (req.header('x-forwarded-proto') !== 'https')
+//       res.redirect(`https://${req.header('host')}${req.url}`)
+//   })
+// }
 
 app.get('/betweenSearch/:fromDate/:toDate/:ticker', async (req, res) => {
   let tempJSON = [];
